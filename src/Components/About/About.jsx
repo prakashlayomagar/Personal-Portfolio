96d651ad50d9f94201661c51d3872e4db40d58f8
prakash <petermagr4@gmail.com>
2026-03-23 12:55:03 -0400

    Updated about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
       <div className={styles.content}>
         <div className={styles.introduction}>
           <p>
-          <br/>Welcome to my corner of the digital realm! I'm<b> Prakash Magar</b>, a passionate <b>web and mobile app developer  </b>based in NYC, USA.
+          <br/>Welcome to my corner of the digital realm! I'm<b> Prakash Magar</b>, a passionate <b>web and mobile app developer  </b>based in Baltimore, MD, USA.
             <br/><br/> With a keen eye for detail and a drive for innovation, I specialize in crafting user-centric digital experiences that blend cutting-edge technology with elegant design. Born in the breathtaking land of Mount Everest, I draw inspiration from the rich cultural heritage and natural beauty of Nepal, infusing my projects with a unique perspective that resonates with audiences worldwide. 
             <br /> <br/> Beyond coding, I am an avid content creator on social media, where I share insights into my development journey and celebrate the diverse tapestry of Nepalese culture. Join me as I continue to push the boundaries of possibility in the digital landscape, one line of code at a time. Let's build something extraordinary together. 
             <br/><br/>Here, the <b>wordcloud</b> represent the list of technologies I'm familiar with.
